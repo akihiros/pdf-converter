e194ee2ebaf67de05a5211c5ce260ae76c3eff46
akihiros <b1516315@planet.kanazawa-it.ac.jp>
2019-10-27 04:15:08 +0900

    デフォルトの出力名を入力名から参照するように変更

--- mypdf2image.py
+++ mypdf2image.py
@@ -14,27 +14,29 @@ python mypdf2image.py input_path output_path
 
 print(help)
 
-# コマンドライン引数からパスを指定可能
+# if no jpg and pdf folders, create them
+os.makedirs('jpg', exist_ok=True)
+os.makedirs('pdf', exist_ok=True)
+
+# Specify the path
 args = sys.argv
 if len(args) > 1:
-    files = args
     if len(args) > 2:
         output = args[2]
         args.pop(-1)
     args.pop(0)
-else:
-    os.makedirs('jpg', exist_ok=True)
-    os.makedirs('pdf', exist_ok=True)
-    args = glob.glob('pdf/*.pdf')
 
-# pdfを画像に変換
-for path in args:
+# If no path is specified, use the default path
+files = args if os.path.exists(args[0]) else glob.glob('pdf/*.pdf')
+
+# pdf to image
+for path in files:
     images = convert_from_path(path)
 
     for i, image in enumerate(images, 1):
         if 'output' in locals():
             image.save(output + '_{}.jpg'.format(i), 'jpeg')
         else:
-            image.save('jpg/sample_{}.jpg'.format(i), 'jpeg')
+            image.save('jpg/{}_{}.jpg'.format(path.replace('pdf/', '').replace('.pdf', ''), i).replace('pdf\\', ''), 'jpeg')
 
     print('{} complete!'.format(path))
