881cc08b6bf2a803ac958a190915cddca2ebbc76
akihiros <b1516315@planet.kanazawa-it.ac.jp>
2020-01-13 14:17:12 +0900

    Update mypdf2image.py
    不親切な実装部分を削除・修正。使用方法を追記。

--- mypdf2image.py
+++ mypdf2image.py
@@ -8,8 +8,11 @@ help = ''' ----------------------------------------
 How to use
 
 python mypdf2image.py input_path output_path
- ... ex: python mypdf2image.py pdf/test.pdf jpg/test
- ... ex: python mypdf2image.py
+ ... ex: python mypdf2image.py pdf\test.pdf jpg\test
+
+default
+ ... input_path: pdf/*.pdf
+ ... output_path: jpg
 --------------------------------------------
 '''
 
@@ -19,31 +22,28 @@ print(help)
 os.makedirs('jpg', exist_ok=True)
 os.makedirs('pdf', exist_ok=True)
 
-# Specify the path
+files = glob.glob('pdf/*.pdf')
+
 args = sys.argv
-if len(args) > 1:
-    if len(args) > 2:
+if len(args) >= 2: # input指定
+    if len(args) >= 3: # output指定
         output = args[2]
         args.pop(-1)
     args.pop(0)
+    files = args
 
-# If no path is specified, use the default path
-# files = args if os.path.exists(args[0]) else glob.glob('pdf/*.pdf')
-
-files = glob.glob('cvpr2018_pdf/*.pdf')
-# files = glob.glob('../clone-code/pytorch_advanced_origin/2_objectdetection/data/cvpr2019/*.pdf')
-
-print(files)
+print('デフォルトではpdfフォルダにある全てのpdfファイルをjpegに変換します。\n変換するファイル一覧：{}'.format(files))
 
 # pdf to image
 for path in files:
     images = convert_from_path(path)
 
     for i, image in enumerate(images, 1):
+        # ファイルが1つ（指定）の場合、指定されたoutput path_連番.jpgで保存
+        # ファイルが複数（デフォルト）の場合、pdf/元のファイル名_連番.jpgを保存
         if 'output' in locals():
             image.save(output + '_{}.jpg'.format(i), 'jpeg')
         else:
-            image.save('cvpr2018_jpg/{}_{}.jpg'.format(path.replace('pdf/', '').replace('.pdf', ''), i).replace('pdf\\', ''), 'jpeg')
-            # image.save('../clone-code/pytorch_advanced_origin/2_objectdetection/data/cvpr2019/{}.png'.format(i), 'png')
+            image.save('jpg/{}_{}.jpg'.format(path.replace('pdf/', '').replace('.pdf', ''), i).replace('pdf\\', ''), 'jpeg')
 
     print('{} complete!'.format(path))
